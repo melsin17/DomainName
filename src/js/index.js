e97b4a12a7a3d7bb82e84f0fedc98dd9af5d533e
Nelson Ramirez <melsin17@yahoo.com>
2020-03-06 00:08:08 +0000

    centered

--- src/js/index.js
+++ src/js/index.js
@@ -8,23 +8,17 @@ import "../style/index.scss";
 
 //console.log("Hello Rigo from the console!");
 window.onload = function() {
-  let pronoun = ["the", "our", "this", "his", "them "];
+  let pronoun = ["the", "our", "this", "his", "them"];
   let adjective = ["great", "best", "big", "elegant", "bald"];
   let noun = ["goat", "man", "mother", "head", "musician"];
-  /*
-  let Myarray;
-
-  Myarray.forEach(pronoun => {
-    console.log(pronoun);
-    console.log(index);
-  }); 
-  */
 
   let Myarray = [];
   for (let i in pronoun)
     for (let j in adjective)
       for (let k in noun)
-        Myarray.push(pronoun[i] + adjective[j] + noun[k] + ".com <br>");
+        Myarray.push(
+          "https://www." + pronoun[i] + adjective[j] + noun[k] + ".com <br>"
+        );
   console.log(Myarray);
   document.querySelector(".Domain-Name").innerHTML = Myarray;
 };
